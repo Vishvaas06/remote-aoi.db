b2296a2cf6e1e73cbb29d7e7cd7894fcb405aa07
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2024-06-03 21:30:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+//Transmittor
+const { Transmitter, DatabaseEvents } = require("@akarui/aoi.db");
+const db = Transmitter.createConnection({
+    path: `aoidb://vishvaas:123456@one.shadowhosting.xyz:19198`,
+    dbOptions: {
+        type: "KeyValue",
+        options: {
+            dataConfig: {
+                path: "./database",
+            },
+        }
+    }
+})
+
+db.on(DatabaseEvents.Connect, () => console.log("Connected to Database"));
+db.on(DatabaseEvents.Disconnect, () => console.log("Disconnected from Database"));
+db.connect();
+
+
+//Receiver
+const { Receiver } = require("@akarui/aoi.db"); //commonjs
+
+const rec = new Receiver({
+databaseType: "KeyValue",
+databaseOptions: {
+dataConfig:{ 
+tables: ["main", "cache"] } },
+host: "0.0.0.0",
+port: 2896,
+userConfig: [{
+username: "vishvaas",
+password: "123456",
+}]
+});
+
+rec.on("connect", () => {
+  console.log("Connected to Bot");
+});
+
+rec.connect();
